395a5f9ca8ad3e71048d3af945efa6a6b95e726f
Jman4190 <mannelly.john@gmail.com>
2021-02-27 13:38:43 -0800

    first commit
    Code to setup our gpt3 chatbot

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, session
+from twilio.twiml.messaging_response import MessagingResponse
+from jabebot import ask, append_interaction_to_chat_log
+
+app = Flask(__name__)
+# if for some reason your conversation with the bot gets weird, change the secret key 
+app.config['SECRET_KEY'] = '89djhf9lhkd93'
+
+@app.route('/jabebot', methods=['POST'])
+def jabe():
+    incoming_msg = request.values['Body']
+    chat_log = session.get('chat_log')
+    answer = ask(incoming_msg, chat_log)
+    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                         chat_log)
+    msg = MessagingResponse()
+    msg.message(answer)
+    return str(msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
