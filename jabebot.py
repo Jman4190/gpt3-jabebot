b725674e0e92e50dbd26860f9e46a14735a20d35
Jman4190 <mannelly.john@gmail.com>
2021-02-27 14:03:07 -0800

    updated jabebot
    Fixed typo for openai

--- jabebot.py
+++ jabebot.py
@@ -5,7 +5,7 @@ import os
 import openai
 
 load_dotenv()
-open.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 completion = openai.Completion()
 
 start_sequence = "\nJabe:"
@@ -24,3 +24,10 @@ def ask(question, chat_log=None):
     presence_penalty=0.6,
     stop=["\n"]
     )
+    story = response['choices'][0]['text']
+    return str(story)
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = session_prompt
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
